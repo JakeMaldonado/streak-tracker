8e9664210c20e478430d538975bee3e63ce0988e
Anis Merchant <anisxmerchant@gmail.com>
2019-07-07 17:36:08 -0400

    refactor: add 'redux' devtools browser support

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,6 +3,8 @@ import streaksReducer from '../reducers/streaks'
 
 const store = createStore(combineReducers({
   streaks: streaksReducer,
-}))
+}),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
